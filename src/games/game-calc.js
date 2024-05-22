2583f05e463ba841f3e6300f4b2e793d611dd0db
dp <petrishindmitry1997@gmail.com>
2024-05-22 17:06:14 +0300

    Fix review notes

--- src/games/game-calc.js
+++ src/games/game-calc.js
@@ -5,22 +5,18 @@ const task = 'What is the result of the expression?';
 const operations = ['+', '-', '*'];
 
 export const getOperation = (num1, num2, operation) => {
-  let answer = 0;
-  if (operation === '+') {
-    answer = num1 + num2;
-  } else if (operation === '-') {
-    answer = num1 - num2;
-  } else if (operation === '*') {
-    answer = num1 * num2;
-  }
-
-  return answer;
+  const operations = {
+    '+': num1 + num2,
+    '-': num1 - num2,
+    '*': num1 * num2,
+  };
+  return operations[operation];
 };
 
 export const getQuestionAndAnswer = () => {
   const num1 = getRandomNumber();
   const num2 = getRandomNumber();
-  const operation = operations[getRandomNumber(0, 2)];
+  const operation = operations[getRandomNumber(0, operations.length)];
   const question = `${num1} ${operation} ${num2}`;
   const answer = getOperation(num1, num2, operation);
 
--- src/games/game-progression.js
+++ src/games/game-progression.js
@@ -3,17 +3,20 @@ import playGame from '../index.js';
 
 const task = 'What number is missing in the progression?';
 
-const progressionLength = 10;
-
-export const getQuestionAndAnswer = () => {
-  const firstNum = getRandomNumber();
-  const progressionDiff = getRandomNumber(1, 10);
-  const hiddenNum = getRandomNumber(0, progressionLength);
+const generateProgression = (firstNum, progressionDiff, progressionLength) => {
   const progression = [];
-
   for (let i = 0; i < progressionLength; i += 1) {
     progression.push(firstNum + progressionDiff * i);
   }
+  return progression;
+};
+
+export const getQuestionAndAnswer = () => {
+  const progressionLength = 10;
+  const firstNum = getRandomNumber(1, 100);
+  const progressionDiff = getRandomNumber(1, 10);
+  const hiddenNum = getRandomNumber(0, progressionLength);
+  const progression = generateProgression(firstNum, progressionDiff, progressionLength);
 
   const answer = progression[hiddenNum];
   progression[hiddenNum] = '..';
